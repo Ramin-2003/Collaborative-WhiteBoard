e855450227dd0bf5cb825e98a8751d6a1a6efa71
Ramin-2003 <raminsarwer@hotmail.com>
2022-12-27 19:15:49 -0500

    Event handler for broadcasting data created

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const { Socket } = require('engine.io');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app); // creating server
 const io = require('socket.io')(http);
 const port = 3000;
 
-app.use("/", express.static(__dirname + "/public"))
+app.use("/", express.static(__dirname + "/public"));
+
+const onConnection = socket => {
+    socket.on("drawing", data => socket.broadcast.emit("drawing", data)); // broadcast data to all clients in room
+};
+
+io.on("connection", onConnection);
 
 http.listen(port, () => {
     console.log(`Server has started in port ${port}.`);
