c4516c25c50d48ac6829ab4f0873b6d685ca85b7
Ramin-2003 <raminsarwer@hotmail.com>
2024-12-01 22:08:25 -0500

    [added frontend IaC]

---
+++ infra/backend.tf
@@ -0,0 +1,7 @@
+# to do: 
+#1: upload to github
+#2: configure cognito pools here in terraform
+#3: output ids to outputs
+#4: read outputs from javascript file and create sign up / sign in page from yt vid watch later
+
+
